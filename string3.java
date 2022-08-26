10bc0a8f814f3469cd442fc3031a723e9f8cb682
nikhilbaliyan72 <112149933+nikhilbaliyan72@users.noreply.github.com>
2022-08-26 11:19:34 +0530

    Create string3.java

---
+++ string3.java
@@ -0,0 +1,12 @@
+/*
+taking input from user what we want to print
+                                             */
+public class my_3_string{
+  //taking the line for taking input from user
+  import java.util.scanner;
+  public static void main(String[] args){
+    scanner sc = new scanner(system.in)
+      string st=sc.nextline;
+    System.out.println(st);
+  }
+}
