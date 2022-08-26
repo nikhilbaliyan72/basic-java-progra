0dfc85a93d02112289489def39327606ec4a5941
nikhilbaliyan72 <112149933+nikhilbaliyan72@users.noreply.github.com>
2022-08-26 10:51:57 +0530

    Create string1.java

---
+++ string1.java
@@ -0,0 +1,17 @@
+public class my_1_string{
+  public static void main(String[] args){
+    String name=new String(original:"Nikhil");
+    System.out.println(name);
+  }
+  
+  
+  
+  //another type to print the name of any person
+  
+  
+  
+  public class my_1_string{
+  public static void main(String[] args){
+    String name="Nikhil";
+    System.out.println(name);
+  }
