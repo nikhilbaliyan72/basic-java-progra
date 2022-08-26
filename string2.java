fc5a943876686621bb518275d2f15860c0901203
nikhilbaliyan72 <112149933+nikhilbaliyan72@users.noreply.github.com>
2022-08-26 11:03:48 +0530

    Create string2.java

---
+++ string2.java
@@ -0,0 +1,9 @@
+/*
+print the value of the variable using java
+                                                  */
+public class my_1_string{
+  public static void main(String[] args){
+    a=56;
+    b=5.666f;
+    System.out.printf("The value of a is %d and The value of b is %f",a,b);
+  }
