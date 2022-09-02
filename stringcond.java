f2c4352654a5e3039655e53f378c0a1982ea839f
nikhilbaliyan72 <112149933+nikhilbaliyan72@users.noreply.github.com>
2022-09-02 11:56:03 +0530

    Create stringcond.java

---
+++ stringcond.java
@@ -0,0 +1,16 @@
+/*
+use if ele
+*/
+public class condition{
+  public static void main(String[] ager){
+    int age = 6;
+    if (age>18) {
+      System.out.println("Yes boy you can drive");
+    }
+    else{
+       System.out.println("no boy you can not drive  yet");
+    }
+  }
+}
+      
+  
