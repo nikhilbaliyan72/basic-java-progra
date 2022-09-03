c234bd9a77e07a15d720541eb2ce9be4097a2342
nikhilbaliyan72 <112149933+nikhilbaliyan72@users.noreply.github.com>
2022-09-03 11:04:41 +0530

    Create stringlog.java

---
+++ stringlog.java
@@ -0,0 +1,89 @@
+/*
+ using logical operators
+ */
+public class stringpr{
+  public static void main (String[] ager){
+    System.out.println("for logical and....");
+    boolean a = true;
+    boolean b = true;
+    if(a&&b){
+    System.out.println(yes);
+    }
+    else{
+     System.out.println(no);  
+    }
+  }
+}
+
+
+
+
+/*
+we can use it for more than 2 values example are givn bellow
+*/
+public class stringpr{
+  public static void main (String[] ager){
+    System.out.println("for logical and...");
+    boolean a = true;
+    boolean b = true;
+    boolean c = true;
+    if(a&&b){
+    System.out.println(yes);
+    }
+    else{
+     System.out.println(no);  
+    }
+  }
+}
+
+
+
+
+
+/*
+for logical 
+*/
+public class stringpr{
+  public static void main (String[] ager){
+    System.out.println("for logical and....");
+    //if any one condtion is true so it print yessss
+    boolean a = true;
+    boolean b = true;
+    if(a || b){
+    System.out.println(yes);
+    }
+    else{
+     System.out.println(no);  
+    }
+  }
+}
+
+
+
+
+/*
+use of logical not operators
+*/
+
+
+
+
+public class stringpr{
+  public static void main (String[] ager){
+    System.out.println("for logical and....");
+    //if any one condtion is true so it print yessss
+    boolean a = false;
+    boolean b = false;
+    //if(a || b){
+   // System.out.println(yes);
+    //}
+    //else{
+     //System.out.println(no);  
+    //}
+    System.out.println("For logical NOT");
+    System.out.println("Not(A) is");
+    System.out.println(!a);
+    System.out.println("Not(b) is");
+    System.out.println(!b);
+  }
+}
