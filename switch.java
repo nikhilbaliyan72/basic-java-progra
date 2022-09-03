d1b18ccec7427cfd594e77e81d3590b9f0859c31
nikhilbaliyan72 <112149933+nikhilbaliyan72@users.noreply.github.com>
2022-09-03 12:22:51 +0530

    Create switch.java

---
+++ switch.java
@@ -0,0 +1,78 @@
+/*
+switch case in java
+*/
+
+public class stringswitch{
+  public static void main (String[] ager){
+    int age;
+    System.out.println("enter your age");
+    Scanner sc = new scanner(System.in);
+    age= sc.nextInt();
+    switch(age){
+      case 18:
+       System.out.println("you are going to become an adult");
+        break;
+      case 21:
+        System.out.println("you are becoe join to job");
+         break;
+      case 60:
+        System.out.println("you are going to get retired");
+        break;
+        defolt:
+        System.out.println("enjoy your life");
+    }
+  }
+}
+
+
+
+/*
+if we givw here character or string so
+*/
+
+
+
+
+public class stringswitch{
+  public static void main (String[] ager){
+    string var = "shubham";
+    switch(var){
+      case "shubham":
+       System.out.println("you are going to become an adult");
+        break;
+      case "versha":
+        System.out.println("you are becoe join to job");
+         break;
+      case "adi":
+        System.out.println("you are going to get retired");
+        break;
+        defolt:
+        System.out.println("enjoy your life");
+    }
+  }
+}
+
+
+
+
+/*
+if change name so print different case
+*/
+public class stringswitch{
+  public static void main (String[] ager){
+    string var = "adi";
+    switch(var){
+      case "shubham":
+       System.out.println("you are going to become an adult");
+        break;
+      case "versha":
+        System.out.println("you are becoe join to job");
+         break;
+      case "adi":
+        System.out.println("you are going to get retired");
+        break;
+        defolt:
+        System.out.println("enjoy your life");
+    }
+  }
+}
