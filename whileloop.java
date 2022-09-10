b93b37ac595551211a544a6db7b2231b5dea3a6b
nikhilbaliyan72 <112149933+nikhilbaliyan72@users.noreply.github.com>
2022-09-10 12:13:07 +0530

    Create whileloop.java

---
+++ whileloop.java
@@ -0,0 +1,17 @@
+/
+  use of whilw loop in java 
+  */
+  
+  
+  
+  class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("using loop");
+        int i=0;
+        while(i<=3){
+            System.out.println(i);
+            i++;
+        }
+       System.out.println("finish while loop"); 
+    }
+}
