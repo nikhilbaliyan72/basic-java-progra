da4cf3d36d953b56b29a50cdb749566b4a292429
nikhilbaliyan72 <112149933+nikhilbaliyan72@users.noreply.github.com>
2022-09-16 10:41:31 +0530

    Create dowhile.java

---
+++ dowhile.java
@@ -0,0 +1,32 @@
+/*use of do while loop in java for runing program easily
+the program we write is print the counting from 0 to 4
+*/
+
+
+
+class helloworld{
+    public static void main(String[] args) {
+        int a=0;
+        do{
+        System.out.println(a);
+        a++;
+          
+        }while(a<5);
+    }
+}
+
+
+
+/* another example of do while loop
+*/
+
+
+class helloworld{
+    public static void main(String[] args) {
+        int a=1;
+        do{
+        System.out.println(a);
+        a++;
+        }while(a<=55);
+    }
+}
