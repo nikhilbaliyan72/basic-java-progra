1c18d6a5de610586bc4bf05a48a0ea53aac95943
nikhilbaliyan72 <112149933+nikhilbaliyan72@users.noreply.github.com>
2022-09-16 11:47:50 +0530

    Create forloop.java

---
+++ forloop.java
@@ -0,0 +1,47 @@
+/*
+use of for loop in java 
+*/
+
+
+class helloworld{
+    public static void main(String[] args) {
+        for(int i=0;i<=10;i++){
+        System.out.println(i);
+        }
+    }
+}
+
+
+
+
+/*
+another example for loop
+/*
+
+
+class helloworld{
+    public static void main(String[] args) {
+        for(int i=0;i<=55;i++){
+        System.out.println(i);
+        }
+    }
+}
+
+
+
+
+
+
+/*
+print odd no using foe loop foest 10
+*/
+
+
+
+class helloworld{
+    public static void main(String[] args) {
+        for(int i=0;i<=10;i++){
+        System.out.println(2*i+1);
+        }
+    }
+}
