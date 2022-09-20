0085bd29d5de259293b1ce36de04350847890113
nikhilbaliyan72 <112149933+nikhilbaliyan72@users.noreply.github.com>
2022-09-20 11:39:29 +0530

    Create evensum.java

---
+++ evensum.java
@@ -0,0 +1,16 @@
+/*
+find the sum of first n even no using for loop
+*/
+class even {
+    public static void main(String[] args) {
+        int sum=0;
+        int n=4;
+        for (int i=0;i<n;i++){
+            sum=sum+(2*i);
+                System.out.print("sum");
+            }
+            System.out.print("sumof even no is");
+            System.out.println(sum);
+        
+    }
+}
