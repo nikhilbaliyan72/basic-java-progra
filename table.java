6d486fa0d7b754dce8d28111533e70c5aa897c32
nikhilbaliyan72 <112149933+nikhilbaliyan72@users.noreply.github.com>
2022-09-20 11:40:29 +0530

    Create table.java

---
+++ table.java
@@ -0,0 +1,13 @@
+/*
+print the table of 5 using java and for loop
+*/
+
+class HelloWorld {
+    public static void main(String[] args) {
+        int n=5;
+        for (int i=1;i<=10;i++){
+            
+                System.out.printf("%d x %d = %d\n",n,i,n*i);
+        }
+    }
+}
