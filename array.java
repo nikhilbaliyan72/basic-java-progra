cb54ede4b76071383a96dce0ff011687ec433598
nikhilbaliyan72 <112149933+nikhilbaliyan72@users.noreply.github.com>
2022-09-23 11:48:27 +0530

    Create array.java

---
+++ array.java
@@ -0,0 +1,64 @@
+/*
+use of array in java through this example
+*/
+
+
+
+class array {
+    public static void main(String[] args) {
+        int [] marks = new int [5];
+        marks[0] = 90;
+        marks[1]=60;
+        marks[2]=70;
+        marks[3] = 55;
+        marks[4]=70;
+        //put the value which marks ou want to show
+        System.out.println(marks[3]);
+        }
+}
+
+
+
+
+
+/*
+another way to write this 
+*/
+
+
+class array {
+    public static void main(String[] args) {
+        int []marks;
+        marks = new int [5];
+        marks[0] = 90;
+        marks[1]=60;
+        marks[2]=70;
+        marks[3] = 55;
+        marks[4]=70;
+        //put the value which marks ou want to show
+        System.out.println(marks[3]);
+        }
+}
+
+
+
+
+
+/*
+another very simple waqy to declare an array and print the marks with the help of array
+*/
+
+
+class HelloWorld {
+    public static void main(String[] args) {
+        int []marks = {90,80,77,99,80};
+      //  marks = new int [5];
+    //    marks[0] = 90;
+      //  marks[1]=60;
+     //   marks[2]=70;
+     //   marks[3] = 55;
+     //   marks[4]=70;
+        //put the value which marks ou want to show
+        System.out.println(marks[3]);
+        }
+}
