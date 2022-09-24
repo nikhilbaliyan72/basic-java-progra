dc95fd888ef717d1e3a62b0cd61dd780a1b3db13
nikhilbaliyan72 <112149933+nikhilbaliyan72@users.noreply.github.com>
2022-09-24 11:35:55 +0530

    Create lengthfind.java

---
+++ lengthfind.java
@@ -0,0 +1,12 @@
+/*
+if we want to find the lenth of an array so we use it given bellow
+*/
+
+
+
+class HelloWorld {
+    public static void main(String[] args) {
+        int [] marks = {90,88,97,86,88};
+        System.out.println(marks.length);
+    }
+}
