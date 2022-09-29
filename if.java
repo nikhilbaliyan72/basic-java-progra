2a1c6873bdbb31f42384fbfa555383ee50f38f21
nikhilbaliyan72 <112149933+nikhilbaliyan72@users.noreply.github.com>
2022-09-29 11:57:00 +0530

    Create if.java

---
+++ if.java
@@ -0,0 +1,63 @@
+/*
+use of if and else statement in java given bellow
+*/
+
+class HelloWorld {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 5;
+        int c;
+        if(a>b){
+            c=a+b;
+        }
+        else{
+             c = (a+b)*5;
+        }
+        
+        
+      //  int a1 = 4;
+        //int b1= 4;
+        //int c1 = (a1+b1)*5;
+        
+        System.out.println(c);
+    }
+}
+
+
+
+
+
+
+/*
+another example
+*/
+
+
+
+class HelloWorld {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 4;
+        int c;
+        if(a>b){
+            c=a+b;
+        }
+        else{
+             c = (a+b)*5;
+        }
+        
+        
+       int a1 = 4;
+       int b1= 3;
+       int c1 ;
+       if(a>b){
+           c1=a1*b1;
+       }
+       else{
+           c1=a1+b1;
+       }
+        System.out.println(c);
+        System.out.println(c1);
+    }
+}
+
